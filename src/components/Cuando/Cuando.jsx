bb691e9f894b18e437c180ee32a98d68219db52c
Mariano Tourné <marianotourne@gmail.com>
2023-09-19 18:29:10 -0300

    message modified -r

--- src/components/Cuando/Cuando.jsx
+++ src/components/Cuando/Cuando.jsx
@@ -4,7 +4,7 @@ export const Cuando = () => {
   return (
     <div className="footerText">
       <p>El sábado 23 de septiembre vamos a estar entre las 12:30 y 18:00 hs</p>
-      <p>Confirmá si venis a almorzarr</p>
+      <p>Confirmá si venis a almorzar</p>
     </div>
   );
 };
